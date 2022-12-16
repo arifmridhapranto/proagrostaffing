8d53cf831d8f0dcb1ebd65eb6255d5a31af6e315
arifmridhapranto <mridhapranto647@gmail.com>
2022-12-17 00:44:49 +0600

    get project from roni vai

---
+++ js/custom.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+	//banner slider js
+	$('.banner-slider').slick({
+		arrows: false,
+		dots:true,
+		autoplay:false,
+	});
+
+	//banner slider js
+	$('.team-slider').slick({
+		arrows: false,
+		slidesToShow: 4,
+	});
+
+	//counter js
+
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+	
+	//brand slider js
+	$('.brand-slider').slick({
+		slidesToShow: 5,
+		prevArrow: '<i class="fa fa-chevron-left previous-arrow" aria-hidden="true"></i>',
+		nextArrow: '<i class="fa fa-chevron-right next-arrow" aria-hidden="true"></i>',
+		
+	});
+
+});
+
+
+
+// venubox js
+$(document).ready(function () {
+	$('.venobox').venobox({
+		bgcolor: '#e23e38',
+	});
+
+	//wow js
+	new WOW().init();
+});
+
+
+
+
+
+$(document).ready(function () {
+	//testimonial
+
+
+	$('.testimonial-slider').slick({
+		arrows: false,
+		dots: true,
+		autoplay: true,
+	});
+
+
+});
+
+
