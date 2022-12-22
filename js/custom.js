4924991c748d98fbe4ace5801269d9b3c24d12e3
arifmridhapranto <mridhapranto647@gmail.com>
2022-12-23 03:38:26 +0600

    slider and cta section done

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,73 @@
 $(document).ready(function () {
+  //banner slider js
+  $(".banner-slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+  });
 
-	//banner slider js
-	$('.banner-slider').slick({
-		arrows: false,
-		dots:true,
-		autoplay:true,
-	});
+  //banner slider js
+  $(".team-slider").slick({
+    arrows: false,
+    slidesToShow: 4,
+  });
 
-	//banner slider js
-	$('.team-slider').slick({
-		arrows: false,
-		slidesToShow: 4,
-	});
+  //counter js
 
-	//counter js
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1000,
+  });
 
-	$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-	});
-
-	//brand slider js
-	$('.brand-slider').slick({
-		slidesToShow: 5,
-		prevArrow: '<i class="fa fa-chevron-left previous-arrow" aria-hidden="true"></i>',
-		nextArrow: '<i class="fa fa-chevron-right next-arrow" aria-hidden="true"></i>',
-
-	});
-
+  //brand slider js
+  $(".brand-slider").slick({
+    slidesToShow: 5,
+    prevArrow:
+      '<i class="fa fa-chevron-left previous-arrow" aria-hidden="true"></i>',
+    nextArrow:
+      '<i class="fa fa-chevron-right next-arrow" aria-hidden="true"></i>',
+  });
 });
-
-
 
 // venubox js
 $(document).ready(function () {
-	$('.venobox').venobox({
-		bgcolor: '#e23e38',
-	});
+  $(".venobox").venobox({
+    bgcolor: "#e23e38",
+  });
 
-	//wow js
-	new WOW().init();
+  //wow js
+  new WOW().init();
 });
 
+// $(document).ready(function () {
+//   //testimonial
 
+//   $(".testimonial-slider").slick({
+//     arrows: false,
+//     dots: true,
+//     autoplay: true,
+//   });
 
-
-
-$(document).ready(function () {
-	//testimonial
-
-
-	$('.testimonial-slider').slick({
-		arrows: false,
-		dots: true,
-		autoplay: true,
-	});
-
-
-});
-
-
+// });
+ $(document).ready(function () {
+   $(".testimonials").owlCarousel({
+     autoplay: true,
+     dots: true,
+     loop: true,
+     speed: 400,
+     margin: 30,
+     nav: true,gi
+     slideBy: 1,
+     responsive: {
+       0: {
+         items: 1,
+       },
+       768: {
+         items: 2,
+       },
+       1025: {
+         items: 3,
+       },
+     },
+   });
+ });
